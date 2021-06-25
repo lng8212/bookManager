265fd8e30dfb46d59da56fdd776e59d1269cc9e4
Long <long05012001@gmail.com>
2021-06-25 17:23:58 +0700

    fix2 layout books_details

--- app/src/main/java/com/example/bookapp/recyclerview/BookDetailsActivity.kt
+++ app/src/main/java/com/example/bookapp/recyclerview/BookDetailsActivity.kt
@@ -47,7 +47,7 @@ class BookDetailsActivity : AppCompatActivity() {
         ratingBar.rating = item.rating
         author.text = item.author
         title.text = item.title
-        pages.text = item.pages.toString()
+        pages.text = item.pages.toString() + " pages | " + item.review.toString() + " review"
         rate.text = item.rating.toString()
         description.text = item.description
     }
